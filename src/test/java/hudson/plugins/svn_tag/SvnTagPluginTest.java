1248089b757e80132dd97c52f8d1dd7b2d7ec3dd
k2nakamura <k2nakamura@dev.java.net>
2008-04-17 20:42:17 +0000

    Updated a test to the new signature.

--- src/test/java/hudson/plugins/svn_tag/SvnTagPluginTest.java
+++ src/test/java/hudson/plugins/svn_tag/SvnTagPluginTest.java
@@ -1,5 +1,7 @@
 package hudson.plugins.svn_tag;
 
+import java.util.HashMap;
+
 import org.testng.annotations.Test;
 import org.testng.annotations.DataProvider;
 
@@ -11,15 +13,15 @@ import org.testng.annotations.DataProvider;
  */
 public class SvnTagPluginTest {
     @Test public void testEvalTagComment() throws Exception {
-        String s = SvnTagPlugin.evalTagComment("Simple tag");
+        String s = SvnTagPlugin.evalTagComment(new HashMap(), "Simple tag");
         assert s.equals("Simple tag") : "Failed simple tag test. Value '" + s + "'";
         System.setProperty("foo", "bar");
-        s = SvnTagPlugin.evalTagComment("Tag with sys props ${sys['foo']}.");
+        s = SvnTagPlugin.evalTagComment(new HashMap(), "Tag with sys props ${sys['foo']}.");
         assert s.equals("Tag with sys props bar.") : "Failed sys prop embedded tag test. Value '" + s + "'";
         String envValue = System.getenv("ENV_FOO");
         if(envValue != null && envValue.equals("env_bar")) {
             System.out.println("Env value '" + envValue + "'");
-            s = SvnTagPlugin.evalTagComment("Tag with env ${env['ENV_FOO']}.");
+            s = SvnTagPlugin.evalTagComment(System.getenv(), "Tag with env ${env['ENV_FOO']}.");
             assert s.equals("Tag with env env_bar.") : "Failed env prop embedded tag test.Value '" + s + "'";
         }
     }
